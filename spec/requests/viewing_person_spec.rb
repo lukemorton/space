50f91a0e2a9a2346b2abe550d030120243fa6ba2
Luke Morton <lukemorton.dev@gmail.com>
2018-04-12 23:02:14 +0100

    Better name

--- spec/requests/viewing_person_spec.rb
+++ spec/requests/viewing_person_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Viewing person' do
-  it 'has 200 code' do
+  it 'returned successfully' do
     get person_url(create(:person))
     assert_response :success
   end
