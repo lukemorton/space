2fa3202f846a2081f0537afcde009442e332fa5b
Luke Morton <lukemorton.dev@gmail.com>
2018-04-14 10:29:52 +0100

    Add radio travel

--- spec/features/space_flight_travel_spec.rb
+++ spec/features/space_flight_travel_spec.rb
@@ -7,18 +7,26 @@ RSpec.feature 'Space flight travel' do
   end
 
   background do
-    create(:location)
+    create_locations
   end
 
   given(:person) { create(:person) }
+  given(:new_location) { create(:location, name: 'New location') }
 
   def when_i_travel_to_a_valid_location
     visit person_url(person)
-    fill_in :travel_location, with: 'New location'
+    choose new_location.name
     click_button
   end
 
   def then_my_location_should_be_updated
-    expect(page).to have_content('New location')
+    expect(page).to have_content(new_location.name)
+  end
+
+  private
+
+  def create_locations
+    create(:location)
+    new_location
   end
 end
