d92da6cb52b23dc488e5eb01ce780903bffdce2d
Luke Morton <lukemorton.dev@gmail.com>
2018-04-14 10:38:33 +0100

    Expect existing location to be checked

--- spec/features/space_flight_travel_spec.rb
+++ spec/features/space_flight_travel_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.feature 'Space flight travel' do
+  scenario 'Before travelling' do
+    when_viewing_controls
+    then_my_current_location_should_be_selected
+  end
+
   scenario 'Travelling' do
     when_i_travel_to_a_valid_location
     then_my_location_should_be_updated
@@ -10,8 +15,17 @@ RSpec.feature 'Space flight travel' do
     create_locations
   end
 
-  given(:person) { create(:person) }
+  given(:location) { create(:location) }
   given(:new_location) { create(:location, name: 'New location') }
+  given(:person) { create(:person, location: location.name) }
+
+  def when_viewing_controls
+    visit person_url(person)
+  end
+
+  def then_my_current_location_should_be_selected
+    expect(page).to have_css('input[type=radio][checked]')
+  end
 
   def when_i_travel_to_a_valid_location
     visit person_url(person)
