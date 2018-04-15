0d9ef755b701272f774874164cd80e22f4f3b72f
Luke Morton <lukemorton.dev@gmail.com>
2018-04-15 09:49:47 +0100

    Add ship to ViewControls

--- lib/space/flight/view_controls.rb
+++ lib/space/flight/view_controls.rb
@@ -1,15 +1,18 @@
+require_relative '../locations/list'
+
 module Space
   module Flight
     class ViewControls
-      Response = Struct.new(:person, :locations)
+      Response = Struct.new(:ship, :person, :locations)
+      Ship = Struct.new(:id)
 
       def initialize(person_gateway:, location_gateway:)
         @person_gateway = person_gateway
         @location_gateway = location_gateway
       end
 
-      def view(person_id)
-        Response.new(person(person_id), locations)
+      def view(ship_id)
+        Response.new(Ship.new(ship_id), person(ship_id), locations)
       end
 
       private
--- spec/lib/space/flight/view_controls_spec.rb
+++ spec/lib/space/flight/view_controls_spec.rb
@@ -3,12 +3,21 @@ require_relative '../../../../lib/space/flight/view_controls'
 RSpec.describe Space::Flight::ViewControls do
   context 'when viewing flight controls' do
     let(:person) { double(name: 'Luke') }
-    let(:person_gateway) { instance_double('Space::Flight::PersonGateway', find: person) }
-
     let(:locations) { [double] }
-    let(:location_gateway) { instance_double('Space::Locations::LocationGateway', all: locations) }
+    let(:ship_id) { 1 }
 
-    subject { described_class.new(person_gateway: person_gateway, location_gateway: location_gateway).view(1) }
+    let(:use_case) do
+      described_class.new(
+        person_gateway: instance_double('Space::Flight::PersonGateway', find: person),
+        location_gateway: instance_double('Space::Locations::LocationGateway', all: locations)
+      )
+    end
+
+    subject { use_case.view(ship_id) }
+
+    it 'should have ship' do
+      expect(subject.ship.id).to eq(ship_id)
+    end
 
     it 'should have person' do
       expect(subject.person.name).to eq('Luke')
