07d9cbcb1ead31c0b0e9f1131eebfa212e36cfaf
Luke Morton <lukemorton.dev@gmail.com>
2018-04-26 18:57:31 +0100

    Reword ship request spec

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -2,5 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Location do
   it { should validate_presence_of(:name) }
+  it { should have_one(:dock) }
   it { should have_many(:establishments) }
 end
--- spec/requests/ship_spec.rb
+++ spec/requests/ship_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe 'Viewing ship' do
-  it 'returned successfully' do
+RSpec.describe 'Ship' do
+  it 'returns successfully' do
     get ship_url(create(:ship, crew: [create(:person)]))
     assert_response :success
   end
 
-  it 'redirect when not in crew' do
+  it 'redirects when person not in crew' do
     create(:person)
     get ship_url(create(:ship, crew: []))
     assert_response :redirect
