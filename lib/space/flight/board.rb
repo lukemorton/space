36ec50d1954d0f83fbd5482a5f95d9f8145cc303
Luke Morton <lukemorton.dev@gmail.com>
2018-07-29 17:37:16 +0100

    Move validation into separate method

--- lib/space/flight/board.rb
+++ lib/space/flight/board.rb
@@ -10,7 +10,7 @@ module Space
       def board(person_id, ship_id)
         ship = ship_gateway.find(ship_id)
 
-        unless ship.has_crew_member_id?(person_id)
+        if can_board_ship?(ship, person_id)
           ship_gateway.add_crew_member(ship_id, person_id)
           Response.new(true)
         else
@@ -21,6 +21,14 @@ module Space
       private
 
       attr_reader :ship_gateway
+
+      def can_board_ship?(ship, person_id)
+        if ship.has_crew_member_id?(person_id)
+          false
+        else
+          true
+        end
+      end
     end
   end
 end
