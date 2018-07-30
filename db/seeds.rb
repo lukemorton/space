8469a4ad6edb49a1634ddd348b98122e2a7d10ab
Luke Morton <lukemorton.dev@gmail.com>
2018-07-30 21:55:47 +0100

    Add multiple crew to ship in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,22 @@ Location.all.each do |location|
   Dock.create!(name: "#{location.name} Dock", location: location)
 end
 
-Person.create!(
-  name: 'Luke',
-  location: london,
-  ship: Ship.create!(
-    dock: london.establishments.first,
-    fuel: Space::Flight::Ship::FUEL_MAX,
-    name: 'Endeavour',
-    location: london
-  )
 User.create!(
   email: 'luke@example.com',
   password: 'password'
 )
+
+ship = Ship.create!(
+  dock: london.establishments.first,
+  fuel: Space::Flight::Ship::FUEL_MAX,
+  name: 'Endeavour',
+  location: london
 )
+
+%i(Luke Gem).each do |name|
+  Person.create!(
+    name: name,
+    location: london,
+    ship: ship
+  )
+end
