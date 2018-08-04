f1c56801c2bab3d1a5c36165bfe5869a41051922
Luke Morton <lukemorton.dev@gmail.com>
2018-08-04 11:04:49 +0100

    WIP

--- lib/space/locations/dock.rb
+++ lib/space/locations/dock.rb
@@ -10,30 +10,84 @@ module Space
 
       def self.from_object(object)
         return if object.nil?
-        attrs = {
+        new(
           id: object.id,
-          ships: object.ships.map { |ship| Space::Flight::Ship.from_object(ship) },
+          location: Location.from_object(object.location),
+          name: object.name,
+          ships: object.ships.map { |ship| Ship.from_object(ship) },
           slug: object.slug
-        }
-        yield(object, attrs) if block_given?
-        new(attrs)
+        )
       end
 
       attr_accessor :id
       attr_accessor :location
+      attr_accessor :name
       attr_accessor :ships
       attr_accessor :slug
 
-      def name
-        'Dock'
-      end
-
-      def location
-        @location or raise LocationNotLoadedError.new
-      end
-
       def to_param
         slug.to_s
+      end
+
+      class Location
+        include ActiveModel::Model
+
+        def self.from_object(object)
+          new(
+            id: object.id,
+            name: object.name,
+            slug: object.slug
+          )
+        end
+
+        attr_accessor :id
+        attr_accessor :name
+        attr_accessor :slug
+
+        def to_param
+          slug
+        end
+      end
+
+      class Ship
+        include ActiveModel::Model
+
+        def self.from_object(object)
+          new(
+            id: object.id,
+            crew: object.crew.map { |member| CrewMember.from_object(member) },
+            name: object.name,
+            slug: object.slug
+          )
+        end
+
+        attr_accessor :id
+        attr_accessor :crew
+        attr_accessor :name
+        attr_accessor :slug
+
+        def has_crew_member_id?(person_id)
+          crew_ids = crew.map(&:id)
+          crew_ids.include?(person_id)
+        end
+
+        def to_param
+          slug
+        end
+
+        class CrewMember
+          include ActiveModel::Model
+
+          def self.from_object(object)
+            new(
+              id: object.id,
+              name: object.name
+            )
+          end
+
+          attr_accessor :id
+          attr_accessor :name
+        end
       end
     end
   end
--- spec/lib/space/locations/dock_gateway_spec.rb
+++ spec/lib/space/locations/dock_gateway_spec.rb
@@ -2,13 +2,26 @@ require_relative '../../../../lib/space/locations/dock_gateway'
 
 RSpec.describe Space::Locations::DockGateway do
   context 'when finding a dock record' do
-    let(:dock_record_slug) { 'dock-at-london' }
+    let(:location_record) do
+      instance_double('Location', id: 1, name: 'London', slug: 'london')
+    end
+
+    let(:person_record) do
+      instance_double('Person', id: 1, name: 'Luke')
+    end
+
+    let(:ship_record) do
+      instance_double('Ship', id: 1, crew: [person_record], name: 'Endeavour', slug: 'endeavour')
+    end
+
+    let(:dock_record_slug) { 'london-dock' }
     let(:dock_record) do
       instance_double(
         'Dock',
         id: 1,
-        location: instance_double('Location', id: 1, establishments: [], name: 'London', slug: 'london'),
-        ships: [],
+        location: location_record,
+        name: 'London Dock',
+        ships: [ship_record],
         slug: dock_record_slug
       )
     end
@@ -19,12 +32,32 @@ RSpec.describe Space::Locations::DockGateway do
       end
     end
 
-    subject { described_class.new(dock_repository: dock_repository).find_by_slug(dock_record_slug) }
+    subject(:dock) { described_class.new(dock_repository: dock_repository).find_by_slug(dock_record_slug) }
 
     it { is_expected.to be_a(Space::Locations::Dock) }
 
-    it 'should return dock with location' do
-      expect(subject.location).to be_a(Space::Locations::Location)
+    it 'has location' do
+      expect(dock.location.name).to eq(location_record.name)
+    end
+
+    it 'has name' do
+      expect(dock.name).to eq(dock_record.name)
+    end
+
+    it 'has ships' do
+      expect(dock.ships.first.name).to eq(ship_record.name)
+    end
+
+    it 'has ships with crew names' do
+      expect(dock.ships.first.crew.first.name).to eq(person_record.name)
+    end
+
+    it 'has slug' do
+      expect(dock.slug).to eq(dock_record.slug)
+    end
+
+    it 'generates param from slug' do
+      expect(dock.to_param).to eq(dock_record.slug)
     end
 
     context 'and dock does not exist' do
