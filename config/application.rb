0e2864b295e86101fa96cf319dbb675a73bba43e
Luke Morton <lukemorton.dev@gmail.com>
2018-10-03 16:31:02 +0100

    Eager load lib in production

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,8 @@ module Space
 
     # Settings in config/environments/* take precedence over those specified here.
     config.autoload_paths << Rails.root.join('lib')
+    config.eager_load_paths << Rails.root.join('lib')
+
     config.exceptions_app = routes
 
     # Application configuration can go into files in config/initializers
