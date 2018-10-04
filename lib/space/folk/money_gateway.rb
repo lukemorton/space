cb2f021db4c57b0b6bfd82e323b10a5943f21474
Luke Morton <lukemorton.dev@gmail.com>
2018-10-04 23:47:32 +0100

    Add spec for MoneyGateway

--- lib/space/folk/money_gateway.rb
+++ lib/space/folk/money_gateway.rb
@@ -1,4 +1,4 @@
-# require_relative 'person'
+require 'money'
 
 module Space
   module Folk
--- spec/lib/space/folk/money_gateway_spec.rb
+++ spec/lib/space/folk/money_gateway_spec.rb
@@ -1,4 +1,52 @@
 require_relative '../../../../lib/space/folk/money_gateway'
 
 RSpec.describe Space::Folk::MoneyGateway do
+  context 'when initializing bank account' do
+    let(:double_entry) { class_double('DoubleEntry', account: nil, transfer: nil) }
+    let(:person) { instance_double('Space::Folk::Person') }
+    let(:seed) { double }
+    let(:bank) { double }
+
+    subject { described_class.new(double_entry: double_entry).initialize_bank(person) }
+
+    it 'should load seed account' do
+      subject
+      expect(double_entry).to have_received(:account).with(:seed)
+    end
+
+    it 'should load bank account' do
+      subject
+      expect(double_entry).to have_received(:account).with(:bank, scope: person)
+    end
+
+    it 'should transfer from seed to personal bank' do
+      allow(double_entry).to receive(:account).with(:seed).and_return(seed)
+      allow(double_entry).to receive(:account).with(:bank, scope: person).and_return(bank)
+
+      subject
+
+      expect(double_entry).to have_received(:transfer).with(Money.new(50_00), a_hash_including(
+        from: seed,
+        to: bank,
+        code: :initialize
+      ))
+    end
+  end
+
+  context 'when getting bank balance' do
+    let(:double_entry) { class_double('DoubleEntry', account: bank) }
+    let(:person) { instance_double('Space::Folk::Person') }
+    let(:bank) { double(balance: Money.new(50_00)) }
+
+    subject { described_class.new(double_entry: double_entry).bank_balance(person) }
+
+    it 'should load bank account' do
+      subject
+      expect(double_entry).to have_received(:account).with(:bank, scope: person)
+    end
+
+    it 'should return bank balance' do
+      expect(subject).to eq(bank.balance)
+    end
+  end
 end
