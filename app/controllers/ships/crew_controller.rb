577898cf21c005e3f0d6e98d678a078d32ff133c
Luke Morton <lukemorton.dev@gmail.com>
2018-10-30 14:31:21 +0000

    Update crew overview to use dedicated use case

--- app/controllers/ships/crew_controller.rb
+++ app/controllers/ships/crew_controller.rb
@@ -1,4 +1,14 @@
 class Ships::CrewController < Ships::BaseController
   def index
+    @crew_overview = view_crew_overview_use_case.view(params.fetch(:ship_id), current_person.id)
+  end
+
+  private
+
+  def view_crew_overview_use_case
+    Space::Flight::ViewCrewOverview.new(
+      person_gateway: person_gateway,
+      ship_gateway: ship_gateway
+    )
   end
 end
